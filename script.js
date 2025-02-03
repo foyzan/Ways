8b7b4945ccd1d1b21763d92a50498be47658a8de
foyzan <foyzanahmedapon01@gmail.com>
2025-02-03 23:22:59 +0600

    hero section and cursor effect done

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
-
-
-
-
-
-
-
-gtl = gsap.timeline()
-gtl.to("#loader",{
-    display:"block"
-})
-gtl.from(".line h1", {
+function loadingAnimation() {
+  gtl = gsap.timeline();
+  gtl.to("#loader", {
+    display: "block",
+  });
+  gtl.from(".line h1", {
     y: 150,
     stagger: 0.25,
     duration: 0.6,
-    delay: 0.5
-})
-gtl.from(".line1-left",{
+    delay: 0.5,
+  });
+  gtl.from(".line1-left", {
     opacity: 0,
     duration: 0.3,
     onStart: function () {
-        const h5timer = document.querySelector(".line1-left h5")
-        let grow = 0
-        let timer = setInterval(function () {
-            if (grow === 100) {
-                clearInterval(timer)
-            }
-            else {
-                grow++
-                h5timer.innerHTML = grow;
-            }
-        }, 40)
-    }
-})
+      const h5timer = document.querySelector(".line1-left h5");
+      let grow = 0;
+      let timer = setInterval(function () {
+        if (grow === 100) {
+          clearInterval(timer);
+        } else {
+          grow++;
+          h5timer.innerHTML = grow;
+        }
+      }, 40);
+    },
+  });
 
-gtl.to(".line3 h2", {
+  gtl.to(".line3 h2", {
     animationName: "anime",
-    opacity:1
-})
+    opacity: 1,
+  });
 
-gtl.to("#loader", {
-    opacity:0,
-    duration:0.4,
-    delay:4
-})
+  gtl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 4,
+  });
 
-gtl.from("#page1",{
+  gtl.from("#page1", {
     delay: 0.2,
-    y:"100vh",
-    opacity:0,
+    y: "100vh",
+    opacity: 0,
     duration: 0.5,
-    ease:Power4
-})
-gtl.to("#loader", {
-   display:"none"
-})
+    ease: Power4,
+  });
+  gtl.to("#loader", {
+    display: "none",
+  });
+}
 
 
-
-
-
-
-
+function cusrEffect(){
+  document.addEventListener("mousemove", function(event){
+    gsap.to("#crsr", {
+      left:event.x,
+      top:event.y
+    })
+  })
+  
+  Shery.makeMagnet(".logo, nav ul li");
+}
+loadingAnimation()
+cusrEffect()
