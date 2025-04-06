bd1758e2de552248733405caf87b83ac50c4770c
foyzan <40073281+foyzan@users.noreply.github.com>
2025-04-06 13:24:00 +0000

    locomotive and scrolltriggerplate added

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function LocomotiveScrollTriggerPlate(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+
 function loadingAnimation() {
   const tl = gsap.timeline();
 
@@ -43,14 +80,17 @@ function loadingAnimation() {
     delay: 0.2,
   });
   tl.from("nav", {
-    opacity:0
+    opacity: 0
   })
-  tl.from("#page1 .hero .hero-line h1",{
+  tl.from("#page1 .hero .hero-line h1", {
     y: 150,
     stagger: 0.25,
     duration: 0.4,
     delay: 0.2,
   })
+  tl.from(".hero, #page2", {
+    opacity:0,
+  },"-=1.2")
 }
 
 function cusrEffect() {
@@ -67,3 +107,4 @@ function cusrEffect() {
 
 loadingAnimation();
 cusrEffect();
+LocomotiveScrollTriggerPlate()
